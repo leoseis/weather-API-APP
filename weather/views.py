a435fc848ddc09fba5dc7f40f70539596f376cf4
leoseis <eboscrib2@yahoo.com>
2023-12-03 18:53:48 +0100

    rendered  first city

--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']         #wruting this only will render an unbound errot 
+    else:
+        city = ''                                 #puting this else statemets removes it
+
+    return render(request, 'index.html',{'city' : city})
 
 
